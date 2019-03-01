20dfd211ffa47bc814475f1641af467d13a89b32
NakkiCalvin <31292872+NakkiCalvin@users.noreply.github.com>
2019-03-01 17:15:29 +0300

    Add project files.

---
+++ NakkiCalvinWeb1/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NakkiCalvinWeb1.Models;
+
+namespace NakkiCalvinWeb1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Animals animal = new Animals("Cow", 3);
+            ViewBag.animalName = animal.getName;
+            ViewBag.animalAge = animal.getAge;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ NakkiCalvinWeb1/Models/Animals.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NakkiCalvinWeb1.Models
+{
+    public class Animals
+    {
+        private String name;
+        private int age;
+
+        public Animals(String name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName
+        {
+            get { return this.name; }
+        }
+
+        public int getAge
+        {
+            get { return this.age; }
+        }
+    }
+}
